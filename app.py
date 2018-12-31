85312fe34d459cd6c8b1f21dc7c858afd3327f74
El-Kabs <ka.babativa@uniandes.edu.co>
2018-12-31 13:50:57 -0500

    Encodeee

--- app.py
+++ app.py
@@ -307,7 +307,7 @@ def principal():
     time2 = time.time()
     print('Function took {:.3f} ms'.format((time2-time1)*1000.0))
     with open('resultado.json', 'w') as f:
-        json.dump(jsonArchivo, f, ensure_ascii=False).encode('utf8').decode('latin1')
+        json.dump(jsonArchivo, f, ensure_ascii=False).encode('utf8')
     jsonArchivo = {"records": []}
 
 @app.route("/")
