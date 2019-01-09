d4cc62abf4303be3f0679238ac8c2fb94d8e637e
El-Kabs <ka.babativa@uniandes.edu.co>
2019-01-09 11:04:20 -0500

    Xd

--- perros.py
+++ perros.py
@@ -7,8 +7,8 @@ def peticionA():
     uidx = str(uuid.uuid4())
     head = 'https://registroapps.uniandes.edu.co/'+uidx
     headers = {'Referer': head, 'User-Agent': 'aaaaa'}
-    cookies = {'PHPSESSID': '4aad3900e1b430911bff3100037b8498'}
-    url = 'https://registroapps.uniandes.edu.co/oferta_cursos/api/get_courses.php?1f76d6492f1e1cbd95732a6b50e9b613&term=201910&ptrm=1&prefix=ADMI'
+    cookies = {'PHPSESSID': 'ba96414e8c79843038974893f072a007'}
+    url = 'https://registroapps.uniandes.edu.co/oferta_cursos/api/get_courses.php?token=f62515ff9b004a927d0e865ac54603d4&term=201910&ptrm=1&prefix=ADMI'
     r = requests.get(url, headers=headers, cookies=cookies)
     print(r.text)
     
@@ -16,8 +16,8 @@ def peticionB():
     uidx = str(uuid.uuid4())
     head = 'https://registroapps.uniandes.edu.co/'+uidx
     headers = {'Referer': head, 'User-Agent': 'aaaaa'}
-    cookies = {'PHPSESSID': '4aad3900e1b430911bff3100037b8498'}
-    url = 'https://registroapps.uniandes.edu.co/oferta_cursos/api/get_courses.php?1f76d6492f1e1cbd95732a6b50e9b613&term=201910&ptrm=1&prefix=ICYA'
+    cookies = {'PHPSESSID': 'ba96414e8c79843038974893f072a007'}
+    url = 'https://registroapps.uniandes.edu.co/oferta_cursos/api/get_courses.php?token=f62515ff9b004a927d0e865ac54603d4&term=201910&ptrm=1&prefix=ICYA'
     r = requests.get(url, headers=headers, cookies=cookies)
     print(r.text)
     
